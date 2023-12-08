b5159639c5b46ca77a6d861637ab6d4fcc14edb0
Emmanuel Okon <emmaudo27@gmail.com>
2023-12-08 17:44:41 +0100

    regext expressions for placing order

--- src/components/products/deliveryForm.jsx
+++ src/components/products/deliveryForm.jsx
@@ -5,15 +5,25 @@ import { fetchCountries } from "../../utils/countries";
 import { CiGlobe } from "react-icons/ci";
 
 const DeliveryForm = () => {
-  const [firstName, setFirstName] = useState("");
+
+
+
+  const [companyName, setCompanyName] = useState("");
   const [lastName, setLastName] = useState("");
   const [mobileNumber, setMobileNumber] = useState("");
   const [email, setEmail] = useState("");
+  const [product, setProduct] = useState("");
+  const [quantity, setQuantity] = useState(1);
   const [streetAddress, setStreetAddress] = useState("");
   const [city, setCity] = useState("");
   const [countries, setCountries] = useState([]);
-  const [zipCode, setZipCode] = useState("");
+  const [zipCode, setZipCode] = useState(0);
   const [selectedCountry, setSelectedCountry] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [emailCheck, setEmailCheck] = useState(false);
+  const [numberCheck, setNumberCheck] = useState(false);
+
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,6 +42,17 @@ const DeliveryForm = () => {
   console.log(countries);
 
   const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const fieldErrorMessages = {
+    companyName: "Please enter your company name.",
+    lastName: "Please enter your last name.",
+    mobileNumber: "Please enter a valid mobile number.",
+    email: "Please enter a valid email address.",
+    streetAddress: "Please enter your street address.",
+    city: "Please enter your city/town.",
+    selectedCountry: "Please select your country.",
+    zipCode: "Please enter your zip code.",
+  };
 
   const warning = () => {
     toast.error("Please fill in all required fields", {
@@ -52,8 +73,31 @@ const DeliveryForm = () => {
   };
 
   const validateForm = () => {
+    const mobileNumberRegex = /^[0-9]\d{2}[2-9](?!11)\d{6}$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    // Check if mobile number is empty or does not match the regex
+    const numberCheck = mobileNumber === "" || !mobileNumberRegex.test(mobileNumber);
+    const emailCheck = email === "" || !emailRegex.test(email);
+
+    setNumberCheck(numberCheck);
+    setEmailCheck(emailCheck);
+
+    if (numberCheck || emailCheck) {
+      if (numberCheck) {
+        setErrorMessage(fieldErrorMessages.mobileNumber);
+      } else {
+        setErrorMessage(fieldErrorMessages.email);
+      }
+      return false;
+    }
+
+
+    setErrorMessage(""); // Clear error message if all validations pass
+
+
     const requiredFields = [
-      firstName,
+      companyName,
       lastName,
       mobileNumber,
       email,
@@ -77,7 +121,7 @@ const DeliveryForm = () => {
     if (validateForm()) {
       success();
 
-      setFirstName("");
+      setCompanyName("");
       setLastName("");
       setMobileNumber("");
       setEmail("");
@@ -87,6 +131,20 @@ const DeliveryForm = () => {
       setZipCode("");
       setFormSubmitted(false);
     }
+
+    if (numberCheck) {
+      setErrorMessage(fieldErrorMessages.mobileNumber);
+      return false;
+    }
+
+    if (emailCheck) {
+      setErrorMessage(fieldErrorMessages.email);
+
+      return false;
+    }
+
+
+
   };
 
   return (
@@ -97,20 +155,28 @@ const DeliveryForm = () => {
           <div className="flex flex-col lg:flex-row lg:gap-4 w-full">
             <div className="flex flex-col gap-1 w-full">
               <label
-                htmlFor="firstName"
+                htmlFor="companyName"
                 className="text-sm text-[#101928] font-semibold mt-3 "
               >
-                First Name
+                Company Name
               </label>
               <input
-                id="firstName"
-                type={firstName}
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                placeholder="First Name"
+                id="companyName"
+                type={companyName}
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
+                placeholder="Company Name"
                 className="appearance-none font-semibold block w-full bg-white text-greyBlack placeholder:font-medium border-2 border-gray-200 rounded-md py-3 px-2 mb3 leading-tight focus:outline-0 focus:ring-0 focus:ringlemonGreen focus:bg-white focus:border-lemonGreen placeholder:text-[#98A2B3] capitalize placeholder:text-sm"
               />
+              {
+                formSubmitted && companyName === "" && (
+                  <span className="text-red-500 text-sm">
+                    {fieldErrorMessages.companyName}
+                  </span>
+                )
+              }
             </div>
+
             <div className="flex flex-col gap-1 w-full">
               <label
                 htmlFor="lastName"
@@ -126,9 +192,15 @@ const DeliveryForm = () => {
                 onChange={(e) => setLastName(e.target.value)}
                 className="appearance-none font-semibold block w-full bg-white text-greyBlack placeholder:font-medium border-2 border-gray-200 rounded-md py-3 px-2 mb3 leading-tight focus:outline-0 focus:ring-0 focus:ringlemonGreen focus:bg-white focus:border-lemonGreen placeholder:text-[#98A2B3] capitalize placeholder:text-sm"
               />
+              {
+                formSubmitted && lastName === "" && (
+                  <span className="text-red-500 text-sm">
+                    {fieldErrorMessages.lastName}
+                  </span>
+                )
+              }
             </div>
           </div>
-
           <div className="flex flex-col lg:flex-row lg:gap-4 w-full">
             <div className="flex flex-col gap-1 w-full">
               <label
@@ -138,13 +210,20 @@ const DeliveryForm = () => {
                 Mobile Number
               </label>
               <input
-                type={mobileNumber}
+                type="text"
                 id="mobileNumber"
                 value={mobileNumber}
                 onChange={(e) => setMobileNumber(e.target.value)}
                 placeholder="Mobile number"
                 className="appearance-none font-semibold block w-full bg-white text-greyBlack placeholder:font-medium border-2 border-gray-200 rounded-md py-3 px-2 mb3 leading-tight focus:outline-0 focus:ring-0 focus:ringlemonGreen focus:bg-white focus:border-lemonGreen placeholder:text-[#98A2B3] placeholder:text-sm"
               />
+              {
+                formSubmitted && numberCheck && (
+                  <span className="text-red-500 text-sm">
+                    {fieldErrorMessages.mobileNumber}
+                  </span>
+                )
+              }
             </div>
             <div className="flex flex-col gap-1 w-full">
               <label
@@ -154,13 +233,20 @@ const DeliveryForm = () => {
                 Email Address
               </label>
               <input
-                type={email}
+                type="text"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 id="emailAddress"
                 placeholder="Enter your address"
                 className="appearance-none font-semibold block w-full bg-white text-greyBlack placeholder:font-medium border-2 border-gray-200 rounded-md py-3 px-2 mb3 leading-tight focus:outline-0 focus:ring-0 focus:ringlemonGreen focus:bg-white focus:border-lemonGreen placeholder:text-[#98A2B3] placeholder:text-sm"
               />
+              {
+                formSubmitted && emailCheck && (
+                  <span className="text-red-500 text-sm">
+                    {fieldErrorMessages.email}
+                  </span>
+                )
+              }
             </div>
           </div>
           <div className="flex flex-col lg:flex-row lg:gap-4 w-full">
@@ -179,6 +265,13 @@ const DeliveryForm = () => {
                 placeholder="Street Address"
                 className="appearance-none font-semibold block w-full bg-white text-greyBlack placeholder:font-medium border-2 border-gray-200 rounded-md py-3 px-2 mb3 leading-tight focus:outline-0 focus:ring-0 focus:ringlemonGreen focus:bg-white focus:border-lemonGreen placeholder:text-[#98A2B3] placeholder:text-sm"
               />
+              {
+                formSubmitted && streetAddress === "" && (
+                  <span className="text-red-500 text-sm">
+                    {fieldErrorMessages.streetAddress}
+                  </span>
+                )
+              }
             </div>
             <div className="flex flex-col gap-1 w-full">
               <label
@@ -195,6 +288,13 @@ const DeliveryForm = () => {
                 placeholder="City/Town"
                 className="appearance-none font-semibold block w-full bg-white text-greyBlack placeholder:font-medium border-2 border-gray-200 rounded-md py-3 px-2 mb3 leading-tight focus:outline-0 focus:ring-0 focus:ringlemonGreen focus:bg-white focus:border-lemonGreen placeholder:text-[#98A2B3] capitalize placeholder:text-sm"
               />
+              {
+                formSubmitted && city === "" && (
+                  <span className="text-red-500 text-sm">
+                    {fieldErrorMessages.city}
+                  </span>
+                )
+              }
             </div>
           </div>
           <div className="flex flex-col lg:flex-row lg:gap-4 w-full">
@@ -242,14 +342,15 @@ const DeliveryForm = () => {
                     </div>
                   }
                 </Listbox.Button>
+
+
                 <Listbox.Options className="w-fit mt-1 absol bg-slate-200 text-gray-800 max-h-[300px] overflow-auto border border-gray-300 rounded shadow-md focus:outline-none scrollbar-none ">
                   {countries.map((country, index) => (
                     <Listbox.Option
                       key={index}
                       value={country.name}
                       className={({ active }) =>
-                        `py-2 px-2 hover:cursor-pointer ${
-                          active ? "bg-lemonGreen text-white" : ""
+                        `py-2 px-2 hover:cursor-pointer ${active ? "bg-lemonGreen text-white" : ""
                         }`
                       }
                     >
@@ -263,9 +364,8 @@ const DeliveryForm = () => {
                             />
                           </div>
                           <span
-                            className={`${
-                              selected ? "font-semibold" : "font-normal"
-                            }`}
+                            className={`${selected ? "font-semibold" : "font-normal"
+                              }`}
                           >
                             {country.name}
                           </span>
@@ -275,6 +375,13 @@ const DeliveryForm = () => {
                   ))}
                 </Listbox.Options>
               </Listbox>
+              {
+                formSubmitted && selectedCountry === "" && (
+                  <span className="text-red-500 text-sm">
+                    {fieldErrorMessages.selectedCountry}
+                  </span>
+                )
+              }
             </div>
             <div className="flex flex-col gap-1 w-full">
               <label
@@ -284,13 +391,20 @@ const DeliveryForm = () => {
                 Zip Code
               </label>
               <input
-                type={zipCode}
+                type="number"
                 value={zipCode}
                 onChange={(e) => setZipCode(e.target.value)}
                 id="zipCode"
                 placeholder="Zip Code"
                 className="appearance-none font-semibold block w-full bg-white text-greyBlack placeholder:font-medium border-2 border-gray-200 rounded-md py-3 px-2 mb3 leading-tight focus:outline-0 focus:ring-0 focus:ringlemonGreen focus:bg-white focus:border-lemonGreen placeholder:text-[#98A2B3] capitalize placeholder:text-sm"
               />
+              {
+                formSubmitted && zipCode === "" && (
+                  <span className="text-red-500 text-sm">
+                    {fieldErrorMessages.zipCode}
+                  </span>
+                )
+              }
             </div>
           </div>
           <button
