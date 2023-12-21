27e9fa23765956605b8b196c6d78d4b9180f4902
Emmanuel Okon <emmaudo27@gmail.com>
2023-12-21 16:08:27 +0100

    fixed preloader screen

--- src/components/preloader/preloader.jsx
+++ src/components/preloader/preloader.jsx
@@ -4,11 +4,11 @@ import Logo from "../../assets/icons/africa.png";
 function CustomPreloader() {
   return (
     <>
-      <div className="flex flex-col w-full justify-center items-center h-screen">
-        <div className="w-[200px] flex justify-center pb-5 ">
+      <div className="flex flex-col w-full justify-center items-center h-screen bg-gray-200">
+        <div className="w-[200px] h-auto flex justify-center pb-3  ">
           <img src={Logo} alt="logo" className="lg:w-[40%] w-[30%] " />
         </div>
-        <div className="flex justify-center items-center">
+        <div className="h-[20px] ">
           <span className="loader"></span>
         </div>
       </div>
